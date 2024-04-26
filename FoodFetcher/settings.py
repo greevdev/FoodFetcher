a01d5f029c9cffd6b1591e192d34921e7fdf6b1d
Georgi Andreev <greev1@protonmail.com>
2024-04-26 22:44:09 +0300

    Add homepage design, create requirements.txt

--- FoodFetcher/settings.py
+++ FoodFetcher/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "FoodFetcher.calorie_counter.apps.CalorieCounterConfig"
+    "FoodFetcher.calorie_counter.apps.CalorieCounterConfig",
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
