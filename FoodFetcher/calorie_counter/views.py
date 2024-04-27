a2983f816b6a97e64dc7d75fbcfa4f76733f7628
Georgi Andreev <greev1@protonmail.com>
2024-04-27 15:36:14 +0300

    Implement Nutrition API

--- FoodFetcher/calorie_counter/views.py
+++ FoodFetcher/calorie_counter/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import requests, os
 
-
-# Create your views here.
 
 def homepage(request):
+    query = 'cum'
+    api_url = f'https://api.api-ninjas.com/v1/nutrition?query={query}'
+    api_key = os.environ['API_KEY']
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+
     return render(request=request, template_name='homepage.html')
