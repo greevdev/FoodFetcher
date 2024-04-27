714aa19ff0b8a39f22ee829a561845c0d3e94612
Georgi Andreev <greev1@protonmail.com>
2024-04-27 18:14:49 +0300

    Integrate Activity Calorie Burn Estimates, Update requirements.txt

--- FoodFetcher/settings.py
+++ FoodFetcher/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     "FoodFetcher.calorie_counter.apps.CalorieCounterConfig",
     'bootstrap5',
+    'mathfilters'
 ]
 
 MIDDLEWARE = [
